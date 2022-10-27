6ed4066d3a3cd040c3e09dfeade7208089944678
kremin12 <cdph12vilotex@gmail.com>
2022-10-27 14:25:28 -0500

    ruta Hola añadida

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/hola',(req, res)=>{
+    res.send('ruta Hola');
+})
 
 app.listen(3000,()=>{
     console.log('Example app listening on port 3000')
